339a3406a0d44fe0f1a2cd1946b42b34c2f077fd
Ben <blhutchings@upei.ca>
2023-04-12 18:19:50 -0300

    Cleanup

--- src/parsers/util/ParserUtils.ts
+++
@@ -1,6 +0,0 @@
-export function getNumber(value?: string) {
-    if(!value) return undefined
-    value = value.replace(/[^0-9]/g, '');
-    const num = parseInt(value)
-    return isNaN(num) ? undefined : num;
-  }
