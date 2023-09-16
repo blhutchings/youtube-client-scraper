759dc12658ee4a384ed31c291b4ebe26bbaf70d3
Ben <blhutchings@upei.ca>
2023-09-16 14:29:55 -0300

    Removed test code

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
 import YouTube from "./YouTube.js";
 import YouTubeGotClient from "./clients/YouTubeGotClient.js";
 export { YouTube, YouTubeGotClient }
-
-const yt = new YouTube(await YouTubeGotClient.createClient());
-const res = await yt.game({browseId: "UCQvWX73GQygcwXOTSf_VDVg", tab: "home"});
-console.log(res)
