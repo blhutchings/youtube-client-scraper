e9fcfc97d816508d680376fcc159782c393247ce
Ben <blhutchings@upei.ca>
2023-09-16 13:34:48 -0300

    Created README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+## Example Usage
+```ts
+const yt = new YouTube(await YouTubeGotClient.createClient());
+const res = await yt.game({browseId: "UCQvWX73GQygcwXOTSf_VDVg", tab: "home"});
+console.log(res);
+```
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 import YouTube from "./YouTube.js";
 import YouTubeGotClient from "./clients/YouTubeGotClient.js";
 export { YouTube, YouTubeGotClient }
+
+const yt = new YouTube(await YouTubeGotClient.createClient());
+const res = await yt.game({browseId: "UCQvWX73GQygcwXOTSf_VDVg", tab: "home"});
+console.log(res)
