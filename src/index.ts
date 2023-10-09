107727104b3d886ed7e85bfd06ee0473d5274a24
Ben <blhutchings@upei.ca>
2023-10-09 10:36:10 -0300

    Export YouTubeConfigExtractor

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import YouTube from "./clients/YouTube.js";
 import YouTubeGotClient from "./clients/YouTubeGotClient.js";
 import { YouTubeConfig } from "./types/YouTubeConfig.js";
+import YouTubeConfigExtractor from "./util/YouTubeConfigExtractor.js";
 
-export { YouTube, YouTubeGotClient, YouTubeConfig }
+export { YouTube, YouTubeGotClient, YouTubeConfig, YouTubeConfigExtractor }
