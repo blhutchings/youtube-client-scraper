a13a217e99a7bb8b73502ffe0133d37b1f5010ec
Ben <blhutchings@upei.ca>
2023-10-14 18:48:30 -0300

    Export Game Channel Schemas

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import YouTube from "./clients/YouTube.js";
 import YouTubeGotClient from "./clients/YouTubeGotClient.js";
+import { Schema$Game } from "./resources/channel/game/Game.js";
+import { Schema$GameAbout } from "./resources/channel/game/GameAbout.js";
+import { Schema$GameHome } from "./resources/channel/game/GameHome.js";
+import { Schema$GameLive } from "./resources/channel/game/GameLive.js";
+import { Schema$GameOfficial } from "./resources/channel/game/GameOfficial.js";
+import { Schema$GameRecent } from "./resources/channel/game/GameRecent.js";
+import { Schema$GameSnippet } from "./resources/channel/game/GameSnippet.js";
 import { YouTubeConfig } from "./types/YouTubeConfig.js";
 import YouTubeConfigExtractor from "./util/YouTubeConfigExtractor.js";
+
+
+export { Schema$Game, Schema$GameSnippet, Schema$GameHome, Schema$GameLive, Schema$GameRecent, Schema$GameOfficial, Schema$GameAbout }
 
 export { YouTube, YouTubeGotClient, YouTubeConfig, YouTubeConfigExtractor }
